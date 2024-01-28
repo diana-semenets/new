bf731d4474a754bda41b718fbede41fcc4396a6c
diana-semenets <110393311+diana-semenets@users.noreply.github.com>
2024-01-28 21:25:28 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -31,3 +31,20 @@ if (e.code === 'Escape' && modal.classList.contains('show')) {
     closeMode();
   }
 });
+
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+       change.target.classList.add('move');
+      }
+    });
+  }
+  
+  let options = {
+    threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('section');
+  
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
